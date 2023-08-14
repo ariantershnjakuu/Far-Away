4e35743fc581b8276dd24883ac3db0f1e96c3048
ariantershnjakuu <at60058@ubt-uni.net>
2023-08-14 14:40:36 +0200

    Created Far-Away project

---
+++ far-away/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a React app that is rendered by a different React app.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
